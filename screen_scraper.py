152c891269450672ee98baf353db363a2e7cc5bf
Ulreg <ullefry@gmail.com>
2022-02-16 20:09:26 +0100

    Update screen_scraper.py

--- screen_scraper.py
+++ screen_scraper.py
@@ -9,5 +9,6 @@ driver.get("http://wiby.me")
 # FULL SCREEN MODE
 driver.maximize_window()
 
+# FIND THE BUTTEN ON THE WEBSITE THAT SAYS "SUPPRICE ME!"
 driver.find_element_by_xpath(xpath="/html/body/form/div[5]/a").click()
 
